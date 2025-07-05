45f2d32695569a7806d08fa611fde3e1b1f5f604
rafimahrus <mahrusrafi@gmail.com>
2025-07-05 17:50:24 +0700

    hilangkan web view demo

--- app/demo/list-formatting/page.tsx
+++
@@ -1,9 +0,0 @@
-import ListFormattingDemo from '../../../components/ListFormattingDemo'
-
-export default function ListDemoPage() {
-  return (
-    <div className="min-h-screen bg-gray-100 py-8">
-      <ListFormattingDemo />
-    </div>
-  )
-}
